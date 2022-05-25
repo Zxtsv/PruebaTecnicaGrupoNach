6beead4fbc414f5381bf17afe6636e8c88774d3f
ztxtsv <bernardo_pprz@comunidad.unam.mx>
2022-05-25 18:15:55 -0500

    Segundo commit

--- src/main/java/com/gruponach/pruebatecnica/controller/EmployeeController.java
+++ src/main/java/com/gruponach/pruebatecnica/controller/EmployeeController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -39,21 +37,21 @@ public class EmployeeController {
         Boolean success = true;
         Long id;
         LocalDate now = LocalDate.now();
-        LocalDate birthdate = Instant.ofEpochMilli(employee.getBirthdate().getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate birthdate = Instant.ofEpochMilli(employee.getBirthdate().getTime()).atZone(ZoneId.systemDefault())
+                .toLocalDate();
 
         Period period = Period.between(birthdate, now);
 
-        if(employeeService.findIdEmployee(employee.getName(),employee.getLast_name()).size() > 0){
-            if(period.getYears() < 18 || jobsService.findJobById(employee.getJob_id()).isPresent()) {
-                success = false;
-                return new ResponseEntity<String>("{\n'id': null"+"\n'success':"+success+"\n}", HttpStatus.BAD_REQUEST);
-            }
+        if (employeeService.findIdEmployee(employee.getName(), employee.getLast_name()).size() > 0
+                || (period.getYears() < 18 || !jobsService.findJobById(employee.getJob_id()).isPresent())) {
+            success = false;
+            return new ResponseEntity<String>("{\n'id': null" + "\n'success':" + success + "\n}",
+                    HttpStatus.BAD_REQUEST);
         }
 
-        
         employeeService.addEmployee(employee);
-        id = employeeService.findIdEmployee(employee.getName(),employee.getLast_name()).get(0).getId();
-        return new ResponseEntity<String>("{\n'id': "+id+"\n'success':"+success+"\n}", HttpStatus.OK);
+        id = employeeService.findIdEmployee(employee.getName(), employee.getLast_name()).get(0).getId();
+        return new ResponseEntity<String>("{\n'id': " + id + "\n'success':" + success + "\n}", HttpStatus.OK);
     }
 
     @GetMapping(path = "getAll/{id}")
@@ -61,12 +59,12 @@ public class EmployeeController {
         return employeeService.findEmployeesWithLambda(jobId);
     }
 
-    @GetMapping(path="getSort/lambda/{id}")
+    @GetMapping(path = "getSort/lambda/{id}")
     public List<Employee> findEmployeeByJobIdLambda(@PathVariable("id") Long jobId) {
         return employeeService.findEmployeeByJobIdLambda(jobId);
     }
 
-    @GetMapping(path="getSort/{id}")
+    @GetMapping(path = "getSort/{id}")
     public List<Employee> findEmployeeByJobId(@PathVariable("id") Long jobId) {
         return employeeService.findEmployeeByJobId(jobId);
     }
